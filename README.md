a6c8102a06563afa6edf2932f07f776cc28fb2c0
real-kwellercat <lucas.rausch.732@k12.friscoisd.org>
2023-02-17 13:52:49 -0600

    added alot more

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # kweller.js
 A Node.JS package for making simple math easier.
 
+# Documentation
+https://lucasrausch.gitbook.io/kweller.js/ (hoping to get a js.org soon!)
--- kweller.js
+++ kweller.js
@@ -1,4 +1,16 @@
-exports.kwellerVER = function() {
-    console.log("Version v1.0.1 of Kweller.js!");
+exports.VERSION = function() {
+    console.log("Version v1.0.2 of Kweller.js!");
+}
+
+exports.add = function(x, y) {
+    const result = x + y;
+
+    return result;
+}
+
+exports.subtract = function(x, y) {
+    const result = x - y;
+
+    return result;
 }
 
